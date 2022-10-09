640fa514d57db1b9a777baff3609055b1ae6f8a3
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-10-09 16:18:39 +0530

    created next()

--- src/utility/useMultistepForm.ts
+++ src/utility/useMultistepForm.ts
@@ -1,3 +1,12 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 
-export function useMultistepForm(steps: ReactElement) {}
+export function useMultistepForm(steps: ReactElement[]) {
+  const [currentStepIndex, setCurrentStateIndex] = useState(0);
+
+  const next = () => {
+    setCurrentStateIndex((i) => {
+      if (i >= steps.length - 1) return 1;
+      return i + 1;
+    });
+  };
+}
