f52a2d8bc20c7e53e926e1819e0b8eaafbb2c531
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-10-12 21:56:00 +0530

    added conditional rendering for next and finish text change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import { useMultistepForm } from './utility/useMultistepForm';
+import React from "react";
+import "./App.css";
+import { useMultistepForm } from "./utility/useMultistepForm";
 
 function App() {
   const { steps, currentStepIndex, step, back, next } = useMultistepForm([
@@ -10,33 +10,41 @@ function App() {
   return (
     <div
       style={{
-        position: 'relative',
-        background: 'white',
-        border: '1px solid black',
-        padding: '1rem',
-        borderRadius: '0.5rem',
-        fontFamily: 'Arial',
+        position: "relative",
+        background: "white",
+        border: "1px solid black",
+        padding: "1rem",
+        borderRadius: "0.5rem",
+        fontFamily: "Arial",
       }}
     >
       <form>
         <div
           style={{
-            position: 'absolute',
-            top: '0.5rem',
-            right: '0.5rem',
+            position: "absolute",
+            top: "0.5rem",
+            right: "0.5rem",
           }}
         >
           {currentStepIndex + 1} / {steps.length}
         </div>
         {step}
-        <div style={{
-          marginTop: '1rem',
-          display: 'flex',
-          gap: '0.5rem',
-          justifyContent: 'flex-end'
-        }}>
-          {currentStepIndex !== 0 && <button type='button' onClick={back}>Back</button>}
-          <button type='button' onClick={next}>Next</button>
+        <div
+          style={{
+            marginTop: "1rem",
+            display: "flex",
+            gap: "0.5rem",
+            justifyContent: "flex-end",
+          }}
+        >
+          {currentStepIndex !== 0 && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="button" onClick={next}>
+            {currentStepIndex === steps.length - 1 ? "Finish" : "Next"}
+          </button>
         </div>
       </form>
     </div>
