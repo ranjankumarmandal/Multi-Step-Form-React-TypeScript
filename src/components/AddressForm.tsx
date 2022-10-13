ffcc22888d4586719345e0ca280582e35a6297d6
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-10-13 22:15:56 +0530

    completed component AddressForm.tsx

--- src/components/AddressForm.tsx
+++ src/components/AddressForm.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-const AddressForm = () => {};
+const AddressForm = () => {
+  return (
+    <>
+      <label>Street</label>
+      <input autoFocus required type="text" />
+      <label>City</label>
+      <input autoFocus required type="text" />
+      <label>State</label>
+      <input autoFocus required type="text" />
+      <label>Zip</label>
+      <input required type="text" />
+    </>
+  );
+};
 
 export default AddressForm;
