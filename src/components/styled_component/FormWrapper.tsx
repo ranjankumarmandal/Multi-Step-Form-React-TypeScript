bb24c2f369a123fa5277ccd6bcca8adde375c443
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-10-14 22:09:52 +0530

    completed implementation for component FormWrapper.tsx

--- src/components/styled_component/FormWrapper.tsx
+++ src/components/styled_component/FormWrapper.tsx
@@ -11,7 +11,16 @@ const FormWrapper = ({ title, children }: formWrapperProps) => {
       <h2 style={{ textAlign: "center", margin: 0, marginBottom: "2rem" }}>
         {title}
       </h2>
-      <div>{children}</div>
+      <div
+        style={{
+          display: "grid",
+          gap: "1rem 0.5rem",
+          justifyContent: "flex-start",
+          gridTemplateColumns: "auto minmac(auto, 400px)",
+        }}
+      >
+        {children}
+      </div>
     </>
   );
 };
