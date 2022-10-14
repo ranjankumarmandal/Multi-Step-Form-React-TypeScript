5f8d57407c351b378cb9efeebeb68fdf09dbc96d
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-10-14 21:47:36 +0530

    completed jsx part for component AccountForm

--- src/components/AccountForm.tsx
+++ src/components/AccountForm.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-const AccountForm = () => {};
+const AccountForm = () => {
+  return (
+    <>
+      <label>Email</label>
+      <input autoFocus required type="email" />
+      <label>Password</label>
+      <input required type="password" />
+    </>
+  );
+};
 
 export default AccountForm;
