f141c0499a98193788fccb9ceb65700fb82b881d
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-10-14 22:26:36 +0530

    complete implementation for component FormWrapper inside component AccountForm

--- src/components/AccountForm.tsx
+++ src/components/AccountForm.tsx
@@ -3,12 +3,12 @@ import FormWrapper from "./styled_component/FormWrapper";
 
 const AccountForm = () => {
   return (
-    <>
+    <FormWrapper title="Account Creation">
       <label>Email</label>
       <input autoFocus required type="email" />
       <label>Password</label>
       <input required type="password" />
-    </>
+    </FormWrapper>
   );
 };
 
