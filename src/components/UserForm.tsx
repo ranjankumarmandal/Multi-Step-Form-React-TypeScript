c9836f115a5ae30cc222358600850d505d5d7754
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-10-16 19:40:11 +0530

    completed definition for props UserFormProps

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import FormWrapper from "./styled_component/FormWrapper";
 
+interface UserFormProps {
+  firstName: string;
+  lastName: string;
+  age: string;
+}
+
 const UserForm = ({ firstName, lastName, age }: UserFormProps) => {
   return (
     <FormWrapper title="User Details">
