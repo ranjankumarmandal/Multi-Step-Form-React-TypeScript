144e14e6e509d11e3f386ac54a374ca99c38df49
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-10-16 21:40:02 +0530

    completed type definition inside component AccountForm.tsx

--- src/components/AccountForm.tsx
+++ src/components/AccountForm.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import FormWrapper from "./styled_component/FormWrapper";
 
+interface BasicProps {
+  email: string;
+  password: string;
+}
+
+interface AccountFormProps extends BasicProps {
+  updateFields: (fields: Partial<BasicProps>) => void;
+}
+
 const AccountForm = () => {
   return (
     <FormWrapper title="Account Creation">
